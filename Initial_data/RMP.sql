90d9a50e5226cccbb08de379781f2601b5f9f2c8
Pblumin <philipblumin@gmail.com>
2021-11-26 22:39:18 -0500

    working on querries and db

--- Initial_data/RMP.sql
+++ Initial_data/RMP.sql
@@ -50,8 +50,10 @@ CREATE TABLE LIKES( lid INTEGER,
 /*SCHOOLS - sid, name*/
 insert into SCHOOL values (1, 'Cooper Union');
 insert into SCHOOL values (2, 'Columbia');
+insert into SCHOOL values (3, 'NYU');
 
 /*Professor - pid, sid, name, existcount*/
+/*Cooper Profs*/
 insert into PROFESSOR values (1, 1, 'Leonid Vulakh', 2);
 insert into PROFESSOR values (2, 1, 'Fred Fontaine', 2);
 insert into PROFESSOR values (3, 1, 'Yahshodhan Risbud', 2);
@@ -62,6 +64,31 @@ insert into PROFESSOR values (7, 1, 'Carl Sable', 2);
 insert into PROFESSOR values (8, 1, 'Eugene Sokolov', 2);
 insert into PROFESSOR values (9, 1, 'William Germano', 2);
 insert into PROFESSOR values (10, 1, 'Neveen Shlayan', 2);
+
+/*Columbia Profs*/
+insert into PROFESSOR values (11, 2, 'Claire Catenaccio', 2);
+insert into PROFESSOR values (12, 2, 'Robert Thurman', 2);
+insert into PROFESSOR values (13, 2, 'Richard Braverman', 2);
+insert into PROFESSOR values (14, 2, 'Herbert Terrace', 2);
+insert into PROFESSOR values (15, 2, 'David Albert', 2);
+insert into PROFESSOR values (16, 2, 'David Yerkes', 2);
+insert into PROFESSOR values (17, 2, 'Richard Howard', 2);
+insert into PROFESSOR values (18, 2, 'Amanda Claybaugh', 2);
+insert into PROFESSOR values (19, 2, 'Rene Testa', 2);
+insert into PROFESSOR values (20, 2, 'Joseph Slaughter', 2);
+
+/*NYU Profs*/
+insert into PROFESSOR values (21, 3, 'Partha Debroy', 2);
+insert into PROFESSOR values (22, 3, 'Julia Keefer', 2);
+insert into PROFESSOR values (23, 3, 'Alan Corns', 2);
+insert into PROFESSOR values (24, 3, 'Hasia Diner', 2);
+insert into PROFESSOR values (25, 3, 'Steve Hutkins', 2);
+insert into PROFESSOR values (26, 3, 'Tyler Volk', 2);
+insert into PROFESSOR values (27, 3, 'Barbara Heyns', 2);
+insert into PROFESSOR values (28, 3, 'Joel Spencer', 2);
+insert into PROFESSOR values (29, 3, 'Phillip Kaim', 2);
+insert into PROFESSOR values (30, 3, 'Steve Blader', 2);
+
 
 
 /*Classes - cid, pid, name*/
@@ -94,11 +121,54 @@ insert into CLASS values (18, 9, 'Opera');
 insert into CLASS values (19, 10, 'Electronics 2');
 insert into CLASS values (20, 10, 'Gastronomy');
 
+insert into CLASS values (21, 11, 'Greek');
+insert into CLASS values (22, 11, 'Theater');
+
+insert into CLASS values (23, 12, 'Physics 201');
+
+insert into CLASS values (24, 13, 'Physics 301');
+
+insert into CLASS values (25, 14, 'Physics 301');
+
+insert into CLASS values (26, 15, 'Ancient Greece');
+
+insert into CLASS values (27, 16, 'Artifical Intelligence');
+
+insert into CLASS values (28, 17, 'Computer Vision');
+
+insert into CLASS values (29, 18, 'Psychology');
+
+insert into CLASS values (30, 19, 'Business 101');
+
+insert into CLASS values (31, 20, 'Creative Writing');
+
+insert into CLASS values (32, 21, 'Biology');
+
+insert into CLASS values (33, 22, 'Film');
+
+insert into CLASS values (34, 23, 'Macro');
+
+insert into CLASS values (35, 24, 'Micro');
+
+insert into CLASS values (36, 25, 'Chemistry');
+
+insert into CLASS values (37, 26, 'E&M Physics');
+
+insert into CLASS values (38, 27, 'Gender Studies');
+
+insert into CLASS values (39, 28, 'How to Economics');
+
+insert into CLASS values (40, 29, 'Intro to Python');
+
+insert into CLASS values (41, 30, 'English');
+
+
+
 
 /*User - uid, uname, upass*/
 insert into USER values (1, 'Awesomephil7', '#ilovedb');
 insert into USER values (2, 'Pcucchi', 'plantplayer');
-insert into USER values (3, 'Winner', 'cuise2022');
+insert into USER values (3, 'Winner', 'cruise2022');
 insert into USER values (4, 'NPR', 'fries');
 
 /*Review - rid, pid, uid, difficuly, overallrating, recommendbool, review */
@@ -118,7 +188,6 @@ insert into REVIEW values (10, 6, 1, 1, 5, 0, 'Awesome guy!');
 insert into REVIEW values (11, 7, 1, 2, 3, 1, 'Awesome guy!');
 insert into REVIEW values (12, 7, 1, 3, 2, 0, 'Awesome guy!');
 insert into REVIEW values (13, 8, 1, 4, 1, 1, 'Awesome guy!');
-
 insert into REVIEW values (14, 9, 2, 2, 3, 1, 'Cool guy!');
 insert into REVIEW values (15, 9, 3, 3, 2, 0, 'Cool guy!');
 insert into REVIEW values (16, 9, 4, 4, 1, 1, 'Cool guy!');
@@ -129,7 +198,18 @@ insert into REVIEW values (20, 7, 1, 2, 3, 1, 'Cool guy!');
 insert into REVIEW values (21, 5, 2, 3, 2, 0, 'Cool guy!');
 insert into REVIEW values (22, 2, 1, 4, 1, 1, 'Cool guy!');
 
+insert into REVIEW values (23, 15, 2, 1, 5, 1, 'Cool!');
+insert into REVIEW values (24, 16, 3, 1, 5, 1, 'Cool guy!');
+insert into REVIEW values (25, 17, 1, 5, 3, 1, 'Do not take this professor');
+insert into REVIEW values (26, 18, 1, 2, 4, 1, 'A lot of assignments!');
+insert into REVIEW values (27, 19, 1, 1, 5, 0, 'Gives no homework!');
+insert into REVIEW values (28, 20, 1, 2, 3, 1, 'Ends class early!');
+insert into REVIEW values (28, 21, 1, 2, 3, 1, 'Awesome guy!');
+insert into REVIEW values (28, 22, 1, 2, 3, 1, 'Awesome guy!');
+insert into REVIEW values (28, 23, 1, 2, 3, 1, 'Awesome guy!');
+
+
 /*LIKES - lid, rid, user*/
 insert into LIKES values (1, 1, 1);
-insert into LIKES values (1, 1, 2);
-insert into LIKES values (1, 1, 3);
+insert into LIKES values (2, 1, 2);
+insert into LIKES values (3, 1, 3);
--- queries.py
+++ queries.py
@@ -56,42 +56,42 @@ def max_likes_id(cursor):
 
 '''
 Queries we are gonna need:
-1. when you click on professors tab see the following:
+DONE 1. when you click on professors tab see the following:
     table that has pname, school, 
     average overall review rating, average difficulty review rating
     order by rating (AND A QUERY THAT REVISES THE ORDER?)
 
-2. when you click on schools tab see the following:
+IN PROGRESS 2. when you click on schools tab see the following:
     table that has school and top 3 professors
     order by school (on front end also will see query 6)
 
-3. Search by professor: (takes in user input)
+DONE 3. Search by professor: (takes in user input)
     query 1 but add a like claus (ie like=%input%)
 
-4. Search by school: (takes in user input)
+DONE 4. Search by school: (takes in user input)
     query 2 but add a like claus
 
-5. Search by class: (takes in user input)
+DONE 5. Search by class: (takes in user input)
     table should be like query 1 
     but you need to do a subquery to get the pid from class using like claus
 
-6. get number of professors at a school
+DONE 6. get number of professors at a school
 
-7. Get a list of all classes taught by a professor
+DONE 7. Get a list of all classes taught by a professor
     (add this under each prof in frontend?)
 
-8. When clicking on a specific school:
+FRONTEND STUFF 8. When clicking on a specific school:
     does query 1
 
-9. When clicking on a specific professor: (takes in input)
+FRONTEND STUFF 9. When clicking on a specific professor: (takes in input)
     on front end see the following info:
     name, classes taught (query 7), 
     avg rating (query 10), avg rating (query 11), % recommend (query 13)
     all reviews (query 12) 
      
-10. get average prof rating (input a prof)
+DONE 10. get average prof rating (input a prof)
 
-11. get average difficulty rating (input a prof)
+DONE 11. get average difficulty rating (input a prof)
 
 12. get all the reviews for a professor (input a prof):
     table that has the follow:
@@ -115,20 +115,206 @@ DONE 14. add a user
 
 '''
 
-# when you click on professors tab see the following:
-#     table that has pname, school, 
-#     average overall review rating, average difficulty review rating
-#     order by rating (AND A QUERY THAT REVISES THE ORDER?)
-
-def load_init_page(cursor):
+# Query 1
+def load_prof_default(cursor):
 
     query = """ SELECT p.pname, s.sname, avg(r.overall) as avgRating, avg(r.difficulty) as avgDiff
                 FROM PROFESSOR p, SCHOOL s, REVIEW r
                 WHERE p.sid=s.sid and r.pid=p.pid and p.existcount=2
                 GROUP BY p.pid
-                ORDER BY avg(r.overall) desc"""
+                ORDER BY avg(r.overall) desc """
 
     cursor.execute(query)
-    prof_init_table = cursor.fetchone()
+    prof_default_table = cursor.fetchone()
 
-    return prof_init_table
+    return prof_default_table
+
+# Query 2
+# 2. when you click on schools tab see the following:
+#     table that has school and top 3 professors
+#     order by school (on front end also will see query 6)
+def load_school_default(cursor):
+    query = """ SELECT s.sname, COUNT(p.pid) as profCnt
+                FROM SCHOOL s, PROFESSOR p
+                WHERE p.sid=s.sid
+                GROUP BY s.sid
+                ORDER BY profCnt desc """
+
+    cursor.execute(query)
+    school_default_table = cursor.fetchone()
+
+    return school_default_table
+
+# NOT WORKING FIGURE OUT
+# http://www.silota.com/docs/recipes/sql-top-n-group.html
+def top_3_prof(cursor):
+
+    query = """ SELECT p.pname, s.sname, avg(r.overall) as avgRating,
+                row_number() over (partition by s.sname order by avgRating desc) as rank 
+                FROM PROFESSOR p, SCHOOL s, REVIEW r
+                WHERE p.sid=s.sid and r.pid=p.pid and p.existcount=2 and rank <= 1"""
+    
+    cursor.execute(query)
+    top_3_prof = cursor.fetchone()
+
+    return top_3_prof
+
+# Query 3       
+def get_prof_by_name(cursor, input):
+    
+    likestr = "'%" + input + "%' "
+    testquery = """ SELECT p.pname, s.sname, avg(r.overall) as avgRating, avg(r.difficulty) as avgDiff
+                FROM PROFESSOR p, SCHOOL s, REVIEW r
+                WHERE p.sid=s.sid and r.pid=p.pid and p.existcount=2 and p.pname LIKE '%Keene%'
+                GROUP BY p.pid
+                ORDER BY avg(r.overall) desc """
+
+    q1 = """SELECT p.pname, s.sname, avg(r.overall) as avgRating, avg(r.difficulty) as avgDiff
+            FROM PROFESSOR p, SCHOOL s, REVIEW r
+            WHERE p.sid=s.sid and r.pid=p.pid and p.existcount=2 and p.pname LIKE """
+    
+    q2 = """GROUP BY p.pid
+            ORDER BY avg(r.overall) desc"""
+
+    query = q1 + likestr + q2 
+
+    cursor.execute(query)
+    prof = cursor.fetchone()
+
+    return prof
+
+# Query 4
+def get_school_by_name(cursor, input):
+
+    likestr = "'%" + input + "%' "
+
+    testquery = """ SELECT s.sname, COUNT(p.pid) as profCnt
+                FROM SCHOOL s, PROFESSOR p
+                WHERE p.sid=s.sid and s.sname LIKE '%Cooper%'
+                GROUP BY s.sid
+                ORDER BY profCnt desc """
+    
+    q1 = """SELECT s.sname, COUNT(p.pid) as profCnt
+            FROM SCHOOL s, PROFESSOR p
+            WHERE p.sid=s.sid and s.sname LIKE"""
+
+    q2 = """ GROUP BY s.sid
+            ORDER BY profCnt desc """
+    
+    query = q1 + likestr + q2 
+
+    cursor.execute(query)
+    school = cursor.fetchone()
+
+    return school
+
+# Query 5
+def get_prof_by_class(cursor, input):
+    likestr = "'%" + input + "%') "
+    
+    testquery = """ SELECT p.pname, s.sname, avg(r.overall) as avgRating, avg(r.difficulty) as avgDiff
+                    FROM PROFESSOR p, SCHOOL s, REVIEW r
+                    WHERE p.sid=s.sid and r.pid=p.pid and p.existcount=2 and 
+                        p.pid = (SELECT c.pid
+                                FROM CLASS c
+                                WHERE c.cname LIKE '%physics%')
+                    GROUP BY p.pid
+                    ORDER BY avg(r.overall) desc """
+    
+    q1 = """SELECT p.pname, s.sname, avg(r.overall) as avgRating, avg(r.difficulty) as avgDiff
+            FROM PROFESSOR p, SCHOOL s, REVIEW r
+            WHERE p.sid=s.sid and r.pid=p.pid and p.existcount=2 and 
+                p.pid = (SELECT c.pid
+                        FROM CLASS c
+                        WHERE c.cname LIKE """
+
+    q2 = """GROUP BY p.pid
+            ORDER BY avg(r.overall) desc"""
+
+    query = q1 + likestr + q2
+
+    cursor.execute(query)
+    prof = cursor.fetchone()
+
+    return prof
+
+# Query 6
+def prof_count(cursor):
+
+    query = """SELECT s.sname, COUNT(p.pid) as profCnt
+                FROM SCHOOL s, PROFESSOR p
+                WHERE s.sid=p.sid
+                GROUP BY s.sid"""
+
+    cursor.execute(query)
+    prof_cnt_table = cursor.fetchone()
+
+    return prof_cnt_table
+
+# Query 7
+def get_classes_by_prof(cursor, input):
+
+    likestr = "'%" + input + "%' "
+    
+    testquery = """SELECT p.pname, c.cname
+                    FROM PROFESSOR p, CLASS c
+                    WHERE c.pid=p.pid and p.pname LIKE '%vul%' """
+
+    q1 = """SELECT p.pname, c.cname
+            FROM PROFESSOR p, CLASS c
+            WHERE c.pid=p.pid and p.pname LIKE """
+
+    query = q1 + likestr
+
+    cursor.execute(query)
+    class_list = cursor.fetchone()
+
+    return class_list
+
+
+# Query 10
+def get_avg_prof_rating(cursor, prof):
+
+    likestr = "'%" + prof + "%' "
+
+    q1 = """SELECT avg(r.overall) as profAvg
+            FROM PROFESSOR p, REVIEW r
+            WHERE p.pid=r.pid and p.pname LIKE """
+    
+    query = q1 + likestr
+    cursor.execute(query)
+    avg_rating = cursor.fetchone()
+
+    return avg_rating
+
+# Query 11
+def get_avg_prof_diff(cursor, prof):
+    likestr = "'%" + prof + "%' "
+
+    q1 = """SELECT avg(r.difficulty) as profAvg
+            FROM PROFESSOR p, REVIEW r
+            WHERE p.pid=r.pid and p.pname LIKE """
+    
+    query = q1 + likestr
+    cursor.execute(query)
+    avg_difficulty = cursor.fetchone()
+
+    return avg_difficulty
+
+# 12. get all the reviews for a professor (input a prof):
+#     table that has the follow:
+#     diff, overall rating, recommend, description, num of likes
+#     order by like count
+def get_avg_prof_diff(cursor, prof):
+    likestr = "'%" + prof + "%' "
+
+    q1 = """SELECT r.diff, r.overall, r.recommend, r.description, r.
+            FROM PROFESSOR p, REVIEW r
+            WHERE p.pid=r.pid and p.pname LIKE"""
+    
+    query = q1 + likestr
+    cursor.execute(query)
+    avg_difficulty = cursor.fetchone()
+
+    return avg_difficulty
+
